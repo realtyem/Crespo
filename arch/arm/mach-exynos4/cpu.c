6c81be189d2d85f7b1010c9fdebfb72f02d65b8c
Kay Sievers <kay.sievers@vrfy.org>
2011-12-21 16:01:38 -0800

    arm: convert sysdev_class to a regular subsystem
    After all sysdev classes are ported to regular driver core entities, the
    sysdev implementation will be entirely removed from the kernel.
    
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Cc: Vinod Koul <vinod.koul@intel.com>
    Cc: Boojin Kim <boojin.kim@samsung.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Lucas De Marchi <lucas.demarchi@profusion.mobi>
    Cc: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Conflicts:
    
    	arch/arm/mach-exynos4/pm.c
    	arch/arm/mach-s3c2410/include/mach/dma.h
    	arch/arm/mach-s5p64x0/pm.c

--- arch/arm/mach-exynos4/cpu.c
+++ arch/arm/mach-exynos4/cpu.c
@@ -9,7 +9,7 @@
 */
 
 #include <linux/sched.h>
-#include <linux/sysdev.h>
+#include <linux/device.h>
 
 #include <asm/mach/map.h>
 #include <asm/mach/irq.h>
@@ -178,17 +178,18 @@ void __init exynos4_init_irq(void)
 	s5p_init_irq(NULL, 0);
 }
 
-struct sysdev_class exynos4_sysclass = {
-	.name	= "exynos4-core",
+struct bus_type exynos4_subsys = {
+	.name		= "exynos4-core",
+	.dev_name	= "exynos4-core",
 };
 
-static struct sys_device exynos4_sysdev = {
-	.cls	= &exynos4_sysclass,
+static struct device exynos4_dev = {
+	.bus	= &exynos4_subsys,
 };
 
 static int __init exynos4_core_init(void)
 {
-	return sysdev_class_register(&exynos4_sysclass);
+	return subsys_system_register(&exynos4_subsys, NULL);
 }
 
 core_initcall(exynos4_core_init);
@@ -222,5 +223,5 @@ int __init exynos4_init(void)
 	/* set idle function */
 	pm_idle = exynos4_idle;
 
-	return sysdev_register(&exynos4_sysdev);
+	return device_register(&exynos4_dev);
 }
--- arch/arm/mach-s3c2410/include/mach/dma.h
+++ arch/arm/mach-s3c2410/include/mach/dma.h
@@ -13,8 +13,7 @@
 #ifndef __ASM_ARCH_DMA_H
 #define __ASM_ARCH_DMA_H __FILE__
 
-#include <plat/dma.h>
-#include <linux/sysdev.h>
+#include <linux/device.h>
 
 #define MAX_DMA_TRANSFER_SIZE   0x100000 /* Data Unit is half word  */
 
@@ -51,6 +50,7 @@ enum dma_ch {
 	DMACH_MAX,		/* the end entry */
 };
 
+#include <plat/dma.h>
 #define DMACH_LOW_LEVEL	(1<<28)	/* use this to specifiy hardware ch no */
 
 /* we have 4 dma channels */
@@ -191,7 +191,7 @@ struct s3c2410_dma_chan {
 	struct s3c2410_dma_buf	*end;		/* end of queue */
 
 	/* system device */
-	struct sys_device	dev;
+	struct device	dev;
 };
 
 typedef unsigned long dma_device_t;

f90cb7554f77713488d3bf1ece48f0ba970466d9
Russell King <rmk+kernel@arm.linux.org.uk>
2012-01-19 15:20:58 +0000

    ARM: fix rcu stalls on SMP platforms
    We can stall RCU processing on SMP platforms if a CPU sits in its idle
    loop for a long time.  This happens because we don't call irq_enter()
    and irq_exit() around generic_smp_call_function_interrupt() and
    friends.  Add the necessary calls, and remove the one from within
    ipi_timer(), so that they're all in a common place.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    
    	arch/arm/kernel/smp.c

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -611,9 +611,9 @@ asmlinkage void __exception_irq_entry do_IPI(int ipinr, struct pt_regs *regs)
 
 	switch (ipinr) {
 	case IPI_TIMER:
-        irq_enter();
+		irq_enter();
 		ipi_timer();
-        irq_exit();
+		irq_exit();
         break;
 
 	case IPI_RESCHEDULE:
@@ -621,21 +621,21 @@ asmlinkage void __exception_irq_entry do_IPI(int ipinr, struct pt_regs *regs)
 		break;
 
 	case IPI_CALL_FUNC:
-        irq_enter();
+		irq_enter();
 		generic_smp_call_function_interrupt();
-        irq_exit();
+		irq_exit();
 		break;
 
 	case IPI_CALL_FUNC_SINGLE:
-        irq_enter();
+		irq_enter();
 		generic_smp_call_function_single_interrupt();
-        irq_exit();
+		irq_exit();
 		break;
 
 	case IPI_CPU_STOP:
-        irq_enter();
+		irq_enter();
 		ipi_cpu_stop(cpu);
-        irq_exit();
+		irq_exit();
 		break;
 
 	case IPI_CPU_BACKTRACE:
